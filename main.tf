726973678e54b7674d4805c68c3dcbb443472792
periboku <ozancankirkyildiz@gmail.com>
2023-03-23 18:40:50 +0300

    datas added

--- main.tf
+++ main.tf
@@ -2,21 +2,29 @@ provider "aws" {
     region = "eu-central-1"
 }
 
+
+
+
+
+
+#########RESOURCES#################
+
 resource "aws_instance" "yaramiyeooo" {
     ami                     = "ami-0499632f10efc5a62"
     instance_type           = "t2.micro"
     vpc_security_group_ids = [aws_security_group.terraformSg.id]
     
-    #user_data = <<-EOF 
-                #!/bin/bash
-    #            echo "Hello World!" > index.html
-    #            nohup busybox httpd -f -p 8080 &
-    #            EOF
+   # user_data = <<-EOF 
+   #             #!/bin/bash
+   #             echo "Hello World!" > index.html
+   #             nohup busybox httpd -f -p $[var.server_port] &
+   #             EOF
 
     tags = {
         Name = "yaramiyeo"
-  }
+    }
 }
+
 
 resource "aws_security_group" "terraformSg" {
     name = "terraform-tutorial-seksenseksen"
@@ -29,8 +37,126 @@ resource "aws_security_group" "terraformSg" {
     }
 }
 
+
+
+resource "aws_launch_configuration" "yukleme_conf" {
+    image_id = aws_instance.yaramiyeooo.ami
+    instance_type = aws_instance.yaramiyeooo.instance_type
+    security_groups = [aws_security_group.terraformSg.id]
+    lifecycle {
+      create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "otoskeyil" {
+    launch_configuration = aws_launch_configuration.yukleme_conf.name
+    min_size = 2
+    max_size = 5
+
+    tag {
+      key = "Name"
+      value = "terraform_asg_ornegi"
+      propagate_at_launch = true
+    }
+}
+
+
+#resource "aws_lb" "loadbalancerim" {
+#  name = "otoskeyil_loadbalancer"
+#  load_balancer_type = "application"
+#  subnets = data.
+#}
+
+
+#########################
+
+
+
+
+
+
+
+
+
+###########DATA############
+
+
+
+#data "aws_vpc" "temel" {
+#    default = true
+#}
+
+
+data "aws_subnets" "benimSubnetler" {
+
+}
+
+
+###############################
+
+
+
+
+
+
+
+
+
+
+
+
+############OUTPUTS#############
+
+output "dis_ip" {
+    value = aws_instance.yaramiyeooo.public_ip
+    description = "bizim makinanın public ip'si"
+}
+
+
+output "sgismi" {
+    value = aws_security_group.terraformSg.arn
+    description = "kullanılan sg'nin ismi"
+  
+}
+
+
+#output "vpcaydi" {
+#  value = data.aws_vpc.temel.id
+#}
+
+output "sabnetler"{
+    value = data.aws_subnets.benimSubnetler
+}
+
+################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###### VARIABLES ####################
+
 variable "server_port" {
     description = "http request için server portu"
     type = number
     default = 8080
 }
+
+####################################
