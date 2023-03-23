5db9eeafa17e0eae8314b618fceeff33c9f53d35
periboku <ozancankirkyildiz@gmail.com>
2023-03-23 15:49:01 +0300

    added variable

--- main.tf
+++ main.tf
@@ -3,9 +3,34 @@ provider "aws" {
 }
 
 resource "aws_instance" "yaramiyeooo" {
-  ami = "ami-0499632f10efc5a62"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "yaramiyeo"
+    ami                     = "ami-0499632f10efc5a62"
+    instance_type           = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.terraformSg.id]
+    
+    #user_data = <<-EOF 
+                #!/bin/bash
+    #            echo "Hello World!" > index.html
+    #            nohup busybox httpd -f -p 8080 &
+    #            EOF
+
+    tags = {
+        Name = "yaramiyeo"
   }
+}
+
+resource "aws_security_group" "terraformSg" {
+    name = "terraform-tutorial-seksenseksen"
+
+    ingress {
+        from_port   = var.server_port
+        to_port     = var.server_port
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] 
+    }
+}
+
+variable "server_port" {
+    description = "http request için server portu"
+    type = number
+    default = 8080
 }
